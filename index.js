aab3b715cbc9b4a4333bcfa8e034cba1a30e829d
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-01-27 01:28:21 -0500

    add port var back in

--- index.js
+++ index.js
@@ -90,6 +90,8 @@ app.post("/api/validate", (req,res) => {
   };
 })
 
-app.listen(process.env.PORT || 5000, () => {
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
   console.log(`App is running on PORT: ${port}.`);
 });
