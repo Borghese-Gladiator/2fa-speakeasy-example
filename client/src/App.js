452d267ca428eea1c5fa4c7bcda3e975aec42fa0
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-02-02 09:15:35 -0500

    set up routes & authTokens

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import PrivateRoute from './context/PrivateRoute';
+import { AuthContext } from "./context/auth";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+// custom components
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Login from './components/Login';
+import Register from './components/Register';
+import Verify from './components/Verify';
+import Validate from './components/Validate';
+
+// /api => welcome
+// /api/register
+// /api/verify
+// /api/validate
 
 function App() {
+  const [authTokens, setAuthTokens] = useState();
+  
+  const setTokens = (data) => {
+    localStorage.setItem("tokens", JSON.stringify(data));
+    setAuthTokens(data);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthContext.Provider value={{ authTokens, setAuthTokens: setTokens }}>
+      <Router>
+        <div>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+            <PrivateRoute path="/verify" component={Verify} />
+            <PrivateRoute path="/validate" component={Validate} />
+          </Switch>
+        </div>
+      </Router>
+    </AuthContext.Provider>
   );
 }
 
