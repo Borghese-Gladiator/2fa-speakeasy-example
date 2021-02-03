4954b3b57501cb83aa52f6f4a7f830c5ea9cc3fe
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-02-02 20:10:09 -0500

    Change so errors from API call only stop rendering of accordion (which uses API call info)

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -34,7 +34,7 @@ export default function Register() {
   const classes = useStyles();
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
-  const [responseObj, setResponseObj] = useState([]);
+  const [responseObj, setResponseObj] = useState(null);
 
   // Note: the empty deps array [] means
   // this useEffect will run once
@@ -44,62 +44,68 @@ export default function Register() {
       .then(res => res.json())
       .then(
         (result) => {
-          setIsLoaded(true);
-          console.log(result)
           setResponseObj(result);
+          setIsLoaded(true);
         },
         // Note: it's important to handle errors here
         // instead of a catch() block so that we don't swallow
         // exceptions from actual bugs in components.
         (error) => {
-          setIsLoaded(true);
           setError(error);
         }
       )
-  }, [])
+  }, []);
 
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  } else if (!isLoaded) {
-    return <div>Loading...</div>;
-  } else {
-    return (
-      <div style={{ color: 'white', background: "linear-gradient(#393e46, #00adb5)"}}>
-        <Container maxWidth="md">
-          <br />
-          <Typography variant="h4" align="center">Registration</Typography>
-          <br />
-          <Accordion>
-            <AccordionSummary
-              expandIcon={<ExpandMoreIcon />}
-              aria-controls="panel1a-content"
-              id="panel1a-header"
-            >
-              <Typography className={classes.heading}>GENERATED USER INFO</Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography>
-                ID: {responseObj.id}
-              </Typography>
-              <Typography>
-                Secret: {responseObj.secret}
-              </Typography>
-            </AccordionDetails>
-          </Accordion>
-          <br />
-          <br />
-          <br />
-          <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center">
-            <Typography variant="body1">Open your Google authenticator app (this can be installed on your phone from Google Play Store for Android and App Store for iOS) and enter the key you just received.</Typography>
-            <br />
-            <Typography variant="body2">Personally, I would copy-paste this to Messenger or Discord and copy it from a message to myself. OR email it to myself to copy paste from Gmail</Typography>
-            <br />
-            <img src={GoogleAuthAppImg} className={classes.instructionsImg} alt="google authenticator app from playstore" />
-          </Box>
-          <br />
-          <br />
-        </Container>
-      </div>
-    );
+  const IDSecretAccordion = () => {
+    console.log(isLoaded);
+    console.log(responseObj);
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography className={classes.heading}>GENERATED USER INFO</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              ID: {responseObj.id}
+            </Typography>
+            <Typography>
+              Secret: {responseObj.secret}
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+      )
+    }
   }
+
+  return (
+    <div style={{ color: 'white', background: "linear-gradient(#393e46, #00adb5)" }}>
+      <Container maxWidth="md">
+        <br />
+        <Typography variant="h4" align="center">Registration</Typography>
+        <br />
+        <IDSecretAccordion />
+        <br />
+        <br />
+        <br />
+        <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center">
+          <Typography variant="body1">Open your Google authenticator app (this can be installed on your phone from Google Play Store for Android and App Store for iOS) and enter the key you just received.</Typography>
+          <br />
+          <Typography variant="body2">Personally, I would copy-paste this to Messenger or Discord and copy it from a message to myself. OR email it to myself to copy paste from Gmail</Typography>
+          <br />
+          <img src={GoogleAuthAppImg} className={classes.instructionsImg} alt="google authenticator app from playstore" />
+        </Box>
+        <br />
+        <br />
+      </Container>
+    </div>
+  );
 }
